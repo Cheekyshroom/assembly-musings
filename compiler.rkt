86ad170bb92a824ec7589bf122029b3c1b01b7d1
Cheekyshroom <phernima@ucsc.edu>
2015-01-15 02:27:38 -0800

    Added string parsing properly, soon inline asm and function application

--- compiler.rkt
+++ compiler.rkt
@@ -9,21 +9,32 @@
 ;;
 
 (module compiler racket/base
-  ;;(require racket/string)
+  (provide (all-defined-out))
 
-  (define (string-index-of str char (start-index 0))
+  (define (index-of-char str char (start-index 0))
     (let ([length (string-length str)])
       (let loop ([i start-index])
-        (if (< i string-length)
+        (if (< i length)
             (if (char=? char (string-ref str i))
                 i
                 (loop (add1 i)))
-            #f)))) 
+            #f))))
 
-  (define (find-substring str substr (start-index 0))
-    (let ([substr (string->list substr)])
-      
-  
+  (define (index-of-substring str substr (start-index 0))
+    (let ([sublist (string->list substr)])
+      (let outer-loop ([start-index 
+                        (index-of-char str (car sublist) start-index)])
+        (if start-index
+            (let inner-loop ([i start-index]
+                             [cs sublist])
+              (cond [(null? cs) start-index]
+                    [(>= i (string-length str)) #f]
+                    [(char=? (car cs) (string-ref str i)) 
+                     (inner-loop (add1 i) (cdr cs))]
+                    [else 
+                     (outer-loop (index-of-char str (car sublist) (add1 i)))]))
+            #f))))
+        
   (define (make-gensym-generator (pconc-name "sym_"))
     (let ([current-value 0])
       (lambda ()
@@ -34,16 +45,20 @@
   (define name-gensym (make-gensym-generator))
   (define loop-gensym (make-gensym-generator "loop_"))
   (define loop-end-gensym (make-gensym-generator "lend_"))
+  (define function-prelude "\tpushq\t%rbx\n\tmovq\t%rsp, %rbx\n")
+  (define function-postlude "\tmovq\t%rbx, %rsp\n\tpopq\t%rbx\n\tret\n")
   
   ;;converts a list into a long string of assembly
   (define (parse-list list)
-    ())
+    list)
      
-  ;;spit string into lists of function applications
+  ;;split string into lists of function applications
   (define (parse-string string)
-    (let* ([start-i (find-substring string "___")]
-           [end-i (find-substring string "___" (add1 start-i))])
+    (let* ([start-i (index-of-substring string "*_")]
+           [end-i (index-of-substring string "_*" (add1 start-i))])
       (if (and start-i end-i) ;;if we have a special directive
           (let ([word (substring string start-i end-i)])
-            (cond [(string=? word "inline") #t]
-                  [(string=? word "code") #t])))))
+            (cond [(string=? word "inline") 
+                  [(string=? word "code") #t]))
+          #t)))
+  )
