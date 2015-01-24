bf59ed74142a5c6aab229d73b34738e3afbf0d3e
Cheekyshroom <phernima@ucsc.edu>
2015-01-24 03:16:27 -0800

    even more concise

--- test.s
+++ test.s
@@ -1,16 +1,10 @@
 (_inline
 	(.data)
-	(string0: .ascii "Zero \0")
-	(string1: .ascii "One  \0")
-	(string2: .ascii "Two  \0")
-	(string3: .ascii "Three\0")
-	(string4: .ascii "Four \0")
-	(string5: .ascii "Five \0")
-	(string6: .ascii "Six  \0")
+	(strings: .ascii "Zero \0" "One  \0" "Two  \0" "Three\0" "Four \0" "Five \0" "Six  \0")
 	(.text))
 
 (_function _print_string_by_number (number)
-	(_print_string (_add (_mul number $6) $string0))
+	(_print_string (_add (_mul number $6) $strings))
 	(_print_newline))
 
 (_function _main_ ()
@@ -22,6 +16,3 @@
 		(decq	%rsi) 
 		(cmpq	$0, %rsi)
 		(jne	_loop_start)))
-
-
-
