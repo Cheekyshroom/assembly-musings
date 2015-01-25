8f4111171feda64ed34aacb6c9fc26adf7699f5a
Cheekyshroom <phernima@ucsc.edu>
2015-01-24 16:36:50 -0800

    added an io example

---
+++ examples/io.s
@@ -0,0 +1,13 @@
+(_comment Reads lines one at a time into a buffer of 512 bytes, until "quit" is entered)
+
+(_inline (.bss) (.lcomm input_buffer, 512) (.data) (quit: .ascii "quit\n\0") (.text))
+
+(_function _main_ ()
+	(read_print_lines))
+
+(_function read_print_lines ()
+	(_read_terminate_string $input_buffer $512)
+	(_print_string $input_buffer)
+	(_if (_equal (_string_compare $quit $input_buffer) $-1)
+		((_identity $20))
+		((read_print_lines))))
