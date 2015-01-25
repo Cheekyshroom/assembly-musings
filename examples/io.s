c82dde9a6bf95352f4da1e9d31bce20604a79708
Cheekyshroom <phernima@ucsc.edu>
2015-01-24 19:04:11 -0800

    updated

--- examples/io.s
+++ examples/io.s
@@ -8,6 +8,6 @@
 (_function read_print_lines ()
 	(_read_terminate_string $input_buffer $512)
 	(_print_string $input_buffer)
-	(_if (_equal (_string_compare $quit $input_buffer) $-1)
+	(_if (_equal_to (_string_compare $quit $input_buffer) $-1)
 		((_identity $20))
 		((read_print_lines))))
